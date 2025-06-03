416af44aed7daae8ea855cae36f0bf1f94c82153
lahcen404 <lahcen.maskour2003@gmail.com>
2025-06-03 17:27:06 +0100

    Add Back and Front end folders

---
+++ PlateShare-BackEnd/src/main/java/com/PlateShare_BackEnd/PlateShare/PlateShareApplication.java
@@ -0,0 +1,13 @@
+package com.PlateShare_BackEnd.PlateShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateShareApplication.class, args);
+	}
+
+}
---
+++ PlateShare-BackEnd/src/test/java/com/PlateShare_BackEnd/PlateShare/PlateShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlateShare_BackEnd.PlateShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0e9dde16b2fec67a0f79ce6afcb44a193638785c
lahcen404 <lahcen.maskour2003@gmail.com>
2025-06-25 14:15:21 +0100

    Create AuthController

--- PlateShare-BackEnd/src/main/java/com/PlateShare_BackEnd/PlateShare/controller/AuthController.java
+++ PlateShare-BackEnd/src/main/java/com/PlateShare_BackEnd/PlateShare/controller/AuthController.java
@@ -1,4 +1,31 @@
 package com.PlateShare_BackEnd.PlateShare.controller;
 
+import com.PlateShare_BackEnd.PlateShare.dto.AuthResponseDTO;
+import com.PlateShare_BackEnd.PlateShare.dto.LoginRequestDTO;
+import com.PlateShare_BackEnd.PlateShare.dto.RegisterDTO;
+import com.PlateShare_BackEnd.PlateShare.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody RegisterDTO registerDTO) {
+        return ResponseEntity.ok(authService.register(registerDTO));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        return ResponseEntity.ok(authService.login(loginRequestDTO));
+    }
 }

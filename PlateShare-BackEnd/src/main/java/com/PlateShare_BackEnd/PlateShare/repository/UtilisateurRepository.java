f6da476d2137e634b88028c779c69c9ccf8b689d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-08-06 16:25:31 +0100

    add Dashboard Stats

--- PlateShare-BackEnd/src/main/java/com/PlateShare_BackEnd/PlateShare/repository/UtilisateurRepository.java
+++ PlateShare-BackEnd/src/main/java/com/PlateShare_BackEnd/PlateShare/repository/UtilisateurRepository.java
@@ -1,5 +1,6 @@
 package com.PlateShare_BackEnd.PlateShare.repository;
 
+import com.PlateShare_BackEnd.PlateShare.enums.Role;
 import com.PlateShare_BackEnd.PlateShare.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface UtilisateurRepository extends JpaRepository<Utilisateur,Long> {
 
     Optional<Utilisateur> findByEmail(String email);
+    long countByRole(Role role);
 }

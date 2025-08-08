14155b08eb0dd1c139c4d70a68bfb7ac84bf4b64
lahcen404 <lahcen.maskour2003@gmail.com>
2025-08-08 17:05:32 +0100

    integrate dashboard Donateur BackEnd with FrontEnd

--- PlateShare-FrontEnd/src/app/features/donateur/donateur-dashboard/donateur-dashboard.ts
+++ PlateShare-FrontEnd/src/app/features/donateur/donateur-dashboard/donateur-dashboard.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {AuthService} from '../../../core/services/auth/AuthService';
 import {Router, RouterLink} from '@angular/router';
+import {DashboardStats} from '../../../core/models/DashboardStats';
+import {DashboardsService} from '../../../core/services/dashboards/dashboards';
 
 @Component({
   selector: 'app-donateur-dashboard',
@@ -10,12 +12,40 @@ import {Router, RouterLink} from '@angular/router';
   templateUrl: './donateur-dashboard.html',
   styleUrl: './donateur-dashboard.css'
 })
-export class DonateurDashboard {
+export class DonateurDashboard implements OnInit{
+  stats:DashboardStats | null=null;
+  error : string | null=null;
+
   constructor(private authService: AuthService,
-              private router: Router){}
+              private router: Router,
+              private dashboardsService: DashboardsService,
+              private cdr:ChangeDetectorRef){}
+
+
+  ngOnInit(): void {
+    this.getDonateurStats();
+  }
+
+  getDonateurStats(): void{
+
+    this.error=null;
+
+    this.dashboardsService.getDonateurStats().subscribe({
+      next:(data)=>{
+        this.stats=data;
+        this.cdr.detectChanges();
+      },
+      error:(err)=>{
+        console.log("Error in loading data !!")
+      }
+    })
+
+  }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
+
+
 }

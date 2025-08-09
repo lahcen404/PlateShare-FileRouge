9a8638ca600c622a448baea1b7f923c0019f7f2e
lahcen404 <lahcen.maskour2003@gmail.com>
2025-08-09 17:33:23 +0100

    add filter by Type Food in List Surplus

--- PlateShare-FrontEnd/src/app/features/donateur/surplus-mylist/surplus-mylist.ts
+++ PlateShare-FrontEnd/src/app/features/donateur/surplus-mylist/surplus-mylist.ts
@@ -1,32 +1,37 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {catchError, Observable, of, tap} from "rxjs";
 import {Surplus} from "../../../core/models/surplus";
 import {SurplusService} from "../../../core/services/surplus/surplus";
 import {SurplusCard} from "../surplus-card/surplus-card";
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../../../core/services/auth/AuthService";
+import {TypeFood} from '../../../core/models/typeFood';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-surplus-mylist',
   imports: [
     SurplusCard,
-    AsyncPipe,
     NgIf,
     NgForOf,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './surplus-mylist.html',
   styleUrl: './surplus-mylist.css'
 })
 export class SurplusMylist implements OnInit{
-  surplus$!: Observable<Surplus[]>;
+  surplus: Surplus[] = [];
+  filtredSurplus: Surplus[] = [];
+  public TypeFoodEnum = TypeFood;
   error: string | null = null;
+  loading = false;
 
-
-  constructor(private surplusService: SurplusService,
-              private authService: AuthService,
-              private router:Router) {}
+  constructor(
+    private surplusService: SurplusService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loadSurplus();
@@ -39,14 +44,33 @@ export class SurplusMylist implements OnInit{
 
   private loadSurplus(): void {
     this.error = null;
+    this.loading = true;
 
-    this.surplus$ = this.surplusService.getAllSurplus().pipe(
-        tap(data => console.log("daaata:", data)),
+    this.surplusService.getAllSurplus().subscribe({
+      next: (data) => {
+        this.surplus = data;
+        console.log("data :",data);
 
-        catchError(err => {
-          this.error = "faailed to load surplus data";
-          return of([]);
-        })
-    );
+        this.filtredSurplus = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = "Failed to load surplus data";
+        this.loading = false;
+        console.error(err);
+      }
+    });
+  }
+
+  onFilterChange(event: Event): void {
+    const filterValue = (event.target as HTMLSelectElement).value;
+    console.log("filter select now is :", filterValue);
+    if (filterValue === 'ALL') {
+      this.filtredSurplus = this.surplus;
+    } else {
+      this.filtredSurplus = this.surplus.filter(
+        surplus => surplus.type === filterValue
+      );
+    }
   }
 }

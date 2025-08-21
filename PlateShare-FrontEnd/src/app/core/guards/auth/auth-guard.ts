1f28703f92d5fda043034c3dc7f34e5aa4225643
lahcen404 <lahcen.maskour2003@gmail.com>
2025-08-21 20:30:38 +0100

    fix something in AuthGuard

--- PlateShare-FrontEnd/src/app/core/guards/auth/auth-guard.ts
+++ PlateShare-FrontEnd/src/app/core/guards/auth/auth-guard.ts
@@ -1,11 +1,31 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
+import {AuthService} from '../../services/auth/AuthService';
 
 export const authGuard: CanActivateFn = () => {
-  const router = inject(Router);
-  if (!localStorage.getItem('token')){
-    router.navigate(['login'])
 
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+
+    // getrole
+    const role = authService.getRole();
+
+    if (role === 'DONATEUR') {
+      router.navigate(['/donateur/dashboard']);
+    } else if (role === 'DEMANDEUR') {
+      router.navigate(['/demandeur/dashboard']);
+    } else if (role === 'ADMIN') {
+      router.navigate(['/admin/dashboard']);
+    } else {
+      router.navigate(['/']);
+    }
+// no access to login & register page
+    return false;
   }
-  return true;
+
+// get access
+return true;
 };
+
